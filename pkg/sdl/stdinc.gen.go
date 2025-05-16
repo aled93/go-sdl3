3cfcb5bd3765192b4b5e02ea0450399a05418a20
AleD <adworld.aled@gmail.com>
2025-05-16 18:10:44 +0900

    Regenerate loader with correct original function name

--- pkg/sdl/stdinc.gen.go
+++ pkg/sdl/stdinc.gen.go
@@ -4,15 +4,15 @@ import "github.com/ebitengine/purego"
 
 func init() {
     registerLoaderFunc(func(lib uintptr) {
-        purego.RegisterLibFunc(&Malloc, lib, "SDL_Malloc")
-        purego.RegisterLibFunc(&Calloc, lib, "SDL_Calloc")
-        purego.RegisterLibFunc(&Realloc, lib, "SDL_Realloc")
-        purego.RegisterLibFunc(&Free, lib, "SDL_Free")
+        purego.RegisterLibFunc(&Malloc, lib, "SDL_malloc")
+        purego.RegisterLibFunc(&Calloc, lib, "SDL_calloc")
+        purego.RegisterLibFunc(&Realloc, lib, "SDL_realloc")
+        purego.RegisterLibFunc(&Free, lib, "SDL_free")
         purego.RegisterLibFunc(&GetOriginalMemoryFunctions, lib, "SDL_GetOriginalMemoryFunctions")
         purego.RegisterLibFunc(&GetMemoryFunctions, lib, "SDL_GetMemoryFunctions")
         purego.RegisterLibFunc(&SetMemoryFunctions, lib, "SDL_SetMemoryFunctions")
-        purego.RegisterLibFunc(&AlignedAlloc, lib, "SDL_AlignedAlloc")
-        purego.RegisterLibFunc(&AlignedFree, lib, "SDL_AlignedFree")
+        purego.RegisterLibFunc(&AlignedAlloc, lib, "SDL_aligned_alloc")
+        purego.RegisterLibFunc(&AlignedFree, lib, "SDL_aligned_free")
         purego.RegisterLibFunc(&GetNumAllocations, lib, "SDL_GetNumAllocations")
         purego.RegisterLibFunc(&GetEnvironment, lib, "SDL_GetEnvironment")
         purego.RegisterLibFunc(&CreateEnvironment, lib, "SDL_CreateEnvironment")
@@ -20,10 +20,10 @@ func init() {
         purego.RegisterLibFunc(&SetEnvironmentVariable, lib, "SDL_SetEnvironmentVariable")
         purego.RegisterLibFunc(&UnsetEnvironmentVariable, lib, "SDL_UnsetEnvironmentVariable")
         purego.RegisterLibFunc(&DestroyEnvironment, lib, "SDL_DestroyEnvironment")
-        purego.RegisterLibFunc(&Getenv, lib, "SDL_Getenv")
-        purego.RegisterLibFunc(&GetenvUnsafe, lib, "SDL_GetenvUnsafe")
-        purego.RegisterLibFunc(&SetenvUnsafe, lib, "SDL_SetenvUnsafe")
-        purego.RegisterLibFunc(&UnsetenvUnsafe, lib, "SDL_UnsetenvUnsafe")
+        purego.RegisterLibFunc(&Getenv, lib, "SDL_getenv")
+        purego.RegisterLibFunc(&GetenvUnsafe, lib, "SDL_getenv_unsafe")
+        purego.RegisterLibFunc(&SetenvUnsafe, lib, "SDL_setenv_unsafe")
+        purego.RegisterLibFunc(&UnsetenvUnsafe, lib, "SDL_unsetenv_unsafe")
         
     })
 }
